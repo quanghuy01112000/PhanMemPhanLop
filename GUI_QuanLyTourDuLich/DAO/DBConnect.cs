d18a57808beb264106af4dff8f6b5133aac621f6
hiiieu <58474777+hiiieu@users.noreply.github.com>
2021-10-22 20:38:08 +0700

    up lần 2
    lần này chắc đúng

---
+++ GUI_QuanLyTourDuLich/DAO/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class DBConnect
+    {
+        //class nào đụng cơ sở dữ liệu thì kế thừa thằng này 
+    }
+}
